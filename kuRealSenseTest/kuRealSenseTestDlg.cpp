95a01f7fe3d38e55d9dfe560ca874a92ae91cc39
Aaron Hsieh <aaron.hsieh@issatek.com>
2019-03-04 16:46:44 +0800

    Open color and depth stream with C++ API.

--- kuRealSenseTest/kuRealSenseTestDlg.cpp
+++ kuRealSenseTest/kuRealSenseTestDlg.cpp
@@ -11,6 +11,10 @@
 #define new DEBUG_NEW
 #endif
 
+#define RS_COLOR_WIDTH  640
+#define RS_COLOR_HEIGHT 480
+#define RS_DEPTH_WIDTH  640
+#define RS_DEPTH_HEIGHT 480
 
 // CAboutDlg dialog used for App About
 
@@ -64,6 +68,8 @@ BEGIN_MESSAGE_MAP(CkuRealSenseTestDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_StartRS, &CkuRealSenseTestDlg::OnBnClickedStartrs)
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 
@@ -99,6 +105,25 @@ BOOL CkuRealSenseTestDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
 	// TODO: Add extra initialization here
+	AllocConsole();
+	FILE * stream;
+	errno_t	err = freopen_s(&stream, "CONOUT$", "w+", stdout);
+
+	cv::namedWindow("ColorView", 0);
+	cv::resizeWindow("ColorView", RS_COLOR_WIDTH, RS_COLOR_HEIGHT);
+	HWND hWndColor = (HWND)cvGetWindowHandle("ColorView");
+	HWND hColorParent = ::GetParent(hWndColor);
+	::SetParent(hWndColor, GetDlgItem(IDC_ColorView)->m_hWnd);
+	::ShowWindow(hColorParent, SW_HIDE);
+
+	cv::namedWindow("DepthView", 0);
+	cv::resizeWindow("DepthView", RS_COLOR_WIDTH, RS_COLOR_HEIGHT);
+	HWND hWndDepth = (HWND)cvGetWindowHandle("DepthView");
+	HWND hDepthParent = ::GetParent(hWndDepth);
+	::SetParent(hWndDepth, GetDlgItem(IDC_DepthView)->m_hWnd);
+	::ShowWindow(hDepthParent, SW_HIDE);
+
+	m_isRSStarted = false;
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -152,3 +177,112 @@ HCURSOR CkuRealSenseTestDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+void CkuRealSenseTestDlg::OnBnClickedStartrs()
+{
+	// TODO: Add your control notification handler code here
+	m_RSConfig.enable_stream(RS2_STREAM_COLOR, RS_COLOR_WIDTH, RS_COLOR_HEIGHT, RS2_FORMAT_BGR8, 30);
+	m_RSConfig.enable_stream(RS2_STREAM_DEPTH, RS_DEPTH_WIDTH, RS_DEPTH_HEIGHT, RS2_FORMAT_Z16, 30);
+	m_RSProfile = m_RSPipeline.start(m_RSConfig);
+
+	m_DepthScale = GetDepthScale(m_RSProfile.get_device());
+	
+	m_isRSStarted = true;
+
+	m_RSColorThread = std::thread(&CkuRealSenseTestDlg::RSThreadFun, this);
+}
+
+rs2_stream CkuRealSenseTestDlg::FindStreamToAlign(const std::vector<rs2::stream_profile>& streams)
+{
+	rs2_stream align_to = RS2_STREAM_ANY;
+	bool depth_stream_found = false;
+	bool color_stream_found = false;
+	for (rs2::stream_profile sp : streams)
+	{
+		rs2_stream profile_stream = sp.stream_type();
+		if (profile_stream != RS2_STREAM_DEPTH)
+		{
+			if (!color_stream_found)         //Prefer color
+				align_to = profile_stream;
+
+			if (profile_stream == RS2_STREAM_COLOR)
+			{
+				color_stream_found = true;
+			}
+		}
+		else
+		{
+			depth_stream_found = true;
+		}
+	}
+
+	if (!depth_stream_found)
+		throw std::runtime_error("No Depth stream available");
+
+	if (align_to == RS2_STREAM_ANY)
+		throw std::runtime_error("No stream found to align with Depth");
+
+	return align_to;
+}
+
+void CkuRealSenseTestDlg::RSThreadFun()
+{
+	rs2::colorizer	color_map;
+
+	rs2_stream		align_to = FindStreamToAlign(m_RSProfile.get_streams());
+	rs2::align		align(align_to);
+	
+	while (m_isRSStarted)
+	{
+		m_RSFrames = m_RSPipeline.wait_for_frames();
+		auto processed = align.process(m_RSFrames);
+
+		rs2::frame color_frame = m_RSFrames.get_color_frame();
+		rs2::frame depth_frame = m_RSFrames.get_depth_frame().apply_filter(color_map);
+		
+		//rs2::video_frame colorFrame = processed.first(align_to);
+		//rs2::depth_frame aligned_depth_frame = processed.get_depth_frame();
+
+		// Creating OpenCV Matrix from a color image
+		cv::Mat colorImg(cv::Size(RS_COLOR_WIDTH, RS_COLOR_HEIGHT), CV_8UC3, (void*)color_frame.get_data(), cv::Mat::AUTO_STEP);
+		cv::Mat depthImg(cv::Size(RS_DEPTH_WIDTH, RS_DEPTH_HEIGHT), CV_8UC3, (void*)depth_frame.get_data(), cv::Mat::AUTO_STEP);
+
+		//cv::Mat colorImg(cv::Size(640, 480), CV_8UC3, (void*)colorFrame.get_data(), cv::Mat::AUTO_STEP);
+		//cv::Mat alignedDepthImg(cv::Size(640, 480), CV_8UC3, (void*)aligned_depth_frame.get_data(), cv::Mat::AUTO_STEP);
+
+		//cv::imshow("ColorView", colorImg);
+		//cv::imshow("DepthView", alignedDepthImg);
+		cv::imshow("ColorView", colorImg);
+		cv::imshow("DepthView", depthImg);
+		cv::waitKey(1);
+	}
+}
+
+float CkuRealSenseTestDlg::GetDepthScale(rs2::device dev)
+{
+	// Go over the device's sensors
+	for (rs2::sensor& sensor : dev.query_sensors())
+	{
+		// Check if the sensor if a depth sensor
+		if (rs2::depth_sensor dpt = sensor.as<rs2::depth_sensor>())
+		{
+			return dpt.get_depth_scale();
+		}
+	}
+	//return 0.0f;
+}
+
+void CkuRealSenseTestDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	// TODO: Add your message handler code here
+	if (m_isRSStarted)
+	{
+		m_isRSStarted = false;
+
+		if (m_RSColorThread.joinable())
+			m_RSColorThread.join();
+
+		m_RSPipeline.stop();
+	}
+}
--- kuRealSenseTest/kuRealSenseTestDlg.h
+++ kuRealSenseTest/kuRealSenseTestDlg.h
@@ -3,7 +3,11 @@
 //
 
 #pragma once
+#include <thread>
+#include <chrono>
 
+#include <opencv2/opencv.hpp>
+#include <librealsense2/rs.hpp>
 
 // CkuRealSenseTestDlg dialog
 class CkuRealSenseTestDlg : public CDialogEx
@@ -20,6 +24,24 @@ public:
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 
+	rs2::pipeline			m_RSPipeline;
+	rs2::pipeline_profile	m_RSProfile;
+	rs2::config				m_RSConfig;
+	rs2::frameset			m_RSFrames;
+
+	cv::Mat					m_RSColorFrame;
+
+	std::thread				m_RSColorThread;
+	std::thread				m_RSDepthThread;
+
+	float					m_DepthScale;
+
+	bool					m_isRSStarted;
+
+	rs2_stream				FindStreamToAlign(const std::vector<rs2::stream_profile> & streams);
+	void					RSThreadFun();
+	float					GetDepthScale(rs2::device dev);
+
 
 // Implementation
 protected:
@@ -31,4 +53,7 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedStartrs();
+	afx_msg void OnDestroy();
 };
